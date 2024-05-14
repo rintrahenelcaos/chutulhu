a8d9f0f09966cada2ffe2fb6c0efa90c3adeb082
Leonardo Mazzeo <leomazzeo@gmail.com>
2024-05-14 11:46:40 -0300

    renew

--- base.py
+++ base.py
@@ -1,4 +1,5 @@
 import pygame
+import math
 
 WIDTH, HEIGHT = 900, 900
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -9,46 +10,154 @@ COLUMNS = 8
 
 BACKGROUND_COLOR = (0,0,0)
 
-BOARD = [((x*CELL, y*CELL), []) for x in range(8) for y in range(8)]
+BOARD = pygame.Surface((800, 800))
+GRID = [(x, y) for x in range(8) for y in range(8)]
 
-def draw_window():
+FPS = 60
+
+chosen_cell = None
+pos_a = pygame.Vector2(0, 0)
+pos_b = pygame.Vector2(200, 300)
+
+def grid_position(position):
+    """translates clicks into discrete cell positions
+
+    Args:
+        position (tuple): coordinates
+
+    Returns:
+        tuple: cell position
+    """
+    
+    x = (position[0]//CELL)#*CELL
+    y = (position[1]//CELL)#*CELL
+    return (x,y)
+
+class GameObject():
+    def __init__(self, speed, xpos, ypos, image) -> None:
+        self.speed = speed
+        self.xpos = xpos
+        self.ypos = ypos
+        self.pos = image.get_rect().move(xpos, ypos)
+    
+    def move(self, up=False, down=False, left=False, right=False):
+        if right:
+            self.pos.right += self.speed
+        if left:
+            self.pos.right -= self.speed
+        if down:
+            self.pos.top += self.speed
+        if up:
+            self.pos.top -= self.speed
+
+def draw_window(pos):
+    
+    global chosen_cell
+    global pos_a
+    
+    WIN.fill(BACKGROUND_COLOR)
+    BOARD.fill("tan4")
     
     
-    #WIN.fill(BACKGROUND_COLOR)
     
+    pos_a = pos_a.move_towards(pos_b,20)
+    print(pos_a[0])
+    #cuadr = pygame.Rect()
+    #pygame.draw.rect(BOARD, "white", pos_a)
+    
+    for row in range(ROWS):
+        for col in range(row % 2, ROWS, 2):
+            pygame.draw.rect(BOARD, "grey3",(CELL*row, CELL*col, CELL,CELL))
+    
+    
+    
+    
+    
+    if chosen_cell != None:
+        pygame.draw.rect(BOARD, (127+waving_func(pygame.time.get_ticks()-chosen_cell[1]),127+waving_func(pygame.time.get_ticks()-chosen_cell[1]),127+waving_func(pygame.time.get_ticks()-chosen_cell[1])), chosen_cell[0])
+    
+    if pos != None:
+        selected_pos = grid_position(pos)
+        #print(selected_pos)
+        #print(grid_position(pos))
+        selected_in_grid = GRID.index(selected_pos)
+        #print(selected_in_grid)
+        #rect = pygame.Rect(CELL*selected_pos[0], CELL*selected_pos[1], CELL, CELL)
+        #print((CELL*selected_pos[0], CELL*selected_pos[1], CELL, CELL))
+        #pygame.draw.rect(BOARD, (255,0,0), (CELL*selected_pos[0], CELL*selected_pos[1], CELL, CELL))
+        chosen_cell = (pygame.Rect(CELL*selected_pos[0], CELL*selected_pos[1], CELL, CELL),pygame.time.get_ticks())
+        print(pygame.time.get_ticks())
+    
+    
+        
+    
+    
+    WIN.blit(BOARD,(0,0))    # actualizes BOARD -> always after all changes odf it
     
     pygame.display.update()
 
-
+def waving_func(time):
+    z = 127*math.cos((2*math.pi/(FPS*40))*time)
+    return z
+    
 def main():
-    """ MMain game function
+    """ Main game function
     """
     
     run = True
-    WIN.fill(BACKGROUND_COLOR)
-    pygame.draw.rect(WIN, "tan4", pygame.Rect(0,0,800,800))
-    for row in range(ROWS):
+    clock = pygame.time.Clock()
+    
+    #WIN.fill(BACKGROUND_COLOR)
+    #BOARD.fill("tan4")
+    
+    
+    #pygame.draw.rect(BOARD, "tan4", pygame.Rect(0,0,800,800))
+    """for row in range(ROWS):
         for col in range(row % 2, ROWS, 2):
-            pygame.draw.rect(WIN, "grey3",(CELL*row, CELL*col, CELL,CELL))
+            pygame.draw.rect(BOARD, "grey3",(CELL*row, CELL*col, CELL,CELL))
+            """
     """for cell in BOARD:
         print(BOARD.index(cell)%2)
         if BOARD.index(cell)%2 == 0:
             pygame.draw.rect(WIN, "white",pygame.Rect(cell[0],cell[1],100,100))
             
         else: pygame.draw.rect(WIN, "red",pygame.Rect(cell[0],cell[1],100,100))"""
+        
+        
+    #WIN.blit(BOARD,(0,0))
+    
+    
     
     while run:
+        
+        clock.tick(FPS)
+        pos = None
+        if BOARD.get_rect().collidepoint(pygame.mouse.get_pos()):
+            pygame.mouse.set_cursor(pygame.cursors.diamond)
+        else: pygame.mouse.set_cursor(pygame.cursors.arrow)
+        
+        
+        """if BOARD.get_rect().collidepoint(pygame.mouse.get_pos()):
+            if pygame.mouse.get_cursor() == pygame.cursors.diamond:
+                pygame.mouse.set_cursor(pygame.cursors.arrow)
+            else: pygame.mouse.set_cursor(pygame.cursors.diamond)"""
         
         for event in pygame.event.get():  
             if event.type == pygame.QUIT:
                 run = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if pygame.mouse.get_cursor()==pygame.cursors.diamond:
+                    pos = pygame.mouse.get_pos()
+                    print("board ", pos)
+        draw_window(pos)
+           
             
                 
             
             
         
         
-        draw_window()
+        
         
     
     pygame.quit()
@@ -56,5 +165,4 @@ def main():
 
 if __name__=="__main__":
     main()
-    print(BOARD)
-    print(len(BOARD))
+    
---
+++ dbcreator.py
@@ -0,0 +1,218 @@
+# importing csv module
+import csv
+import sqlite3
+
+# csv file name
+filename = "cards.csv"
+csvs = ["units.csv", "cards.csv", "spells.csv"]
+#csvs = ["units.csv", "cards.csv"]#, "spells.csv"]
+
+# initializing the titles and rows list
+fields = []
+rows = []
+
+# list of tables
+list_tables = ["units", "cards", "spells"]
+#list_tables = ["units", "cards"]#, "spells"]
+def csvlistconverter(filename):
+# csv file name
+    
+
+# initializing the titles and rows list
+    fields = []
+    rows = []
+
+    # reading csv file
+    with open(filename, "r") as csvfile:
+        # creating a csv reader object
+        csvreader = csv.reader(csvfile)
+
+        # extracting field names through first row
+        fields = next(csvreader)
+
+        # extracting each data row one by one
+        for row in csvreader:
+            rows.append(row)
+
+        
+    
+    return fields, rows
+
+def cardextraction(cardsroute):
+    decki = []
+    
+    notcare, individual = csvlistconverter(cardsroute)
+    for cd in individual:
+        if cd[2] != "":
+            cou = cd[2]
+            cd.pop(2)
+            for num in range(int(cou)):
+                
+                name = cd[0]+str(num)
+                temp = cd[1:]
+                temp.insert(0,name)
+                decki.append(temp)
+                
+                
+        else:
+            cd.pop(2)
+            decki.append(cd)
+    
+    
+                
+    return decki
+
+def repeated_token_extraction(tokenlist):
+    
+    token_out = []
+    for token in tokenlist:
+        
+        for nr in range(int(token[1])):
+            name = token[0]+str(nr)
+            temp = token[2:]
+            temp.insert(0,name)
+            token_out.append(temp)
+    return token_out
+
+
+def conection_sql():
+    global conector
+    conector = sqlite3.connect("currentgame.db")
+    return conector
+
+def tabledropper(conection):
+    pointer = conection.cursor()
+    dropping = "DROP TABLE IF EXISTS deck"
+    pointer.execute(dropping)
+    conection.commit()
+    dropping = "DROP TABLE IF EXISTS images"
+    pointer.execute(dropping)
+    conection.commit()
+
+    
+
+def tableconstructor(conection):
+    pointer = conection.cursor()
+    table = "CREATE TABLE IF NOT EXISTS deck(id INTEGER PRIMARY KEY AUTOINCREMENT, card TEXT NOT NULL, type TEXT NOT NULL, income INTEGER, power INTEGER,  agrogen INTEGER,  defenders INTEGER, mining INTEGER, refinerie INTEGER, colonies INTEGER, labs INTEGER, notes TEXT, force INTEGER, hits INTEGER, placement TEXT, deckpos INTEGER, hitted INTEGER  )"
+    pointer.execute(table)
+    conection.commit()
+    tableimages = "CREATE TABLE IF NOT EXISTS images(id INTEGER PRIMARY KEY AUTOINCREMENT, card TEXT NOT NULL, pict TEXT, descript TEXT )"
+    pointer.execute(tableimages)
+    conection.commit()
+    
+def tableconstructor(conection, fields, table_name,selection = None, ):
+    
+    try:
+        fields.remove("Nbr")
+    except: pass
+    pointer = conection.cursor()
+    table = "CREATE TABLE IF NOT EXISTS "+table_name+"(id INTEGER PRIMARY KEY AUTOINCREMENT, %s TEXT )" % " TEXT, ".join(fields)
+    
+    
+    pointer.execute(table)
+    conection.commit()
+
+def alltablesconstructor(conection):
+    
+    for ind in range(len(csvs)):
+        fields, rows = csvlistconverter(csvs[ind])
+        try:
+            fields.remove("Nbr")
+            rows =repeated_token_extraction(rows)
+        except: pass
+        pointer = conection.cursor()
+        table = "CREATE TABLE IF NOT EXISTS "+list_tables[ind]+"(id INTEGER PRIMARY KEY AUTOINCREMENT, %s TEXT )" % " TEXT, ".join(fields)
+        
+        pointer.execute(table)
+        conection.commit()
+        
+        for row in rows:
+            
+            preload = "INSERT INTO "+list_tables[ind]+"(%s) VALUES (" % ",".join(fields)
+            
+            preload = preload + "?,"*len(fields)
+            preload = preload[:-1]
+            preload =  preload + ")"
+            
+            preload2 = []
+            for discrete in row:
+                preload2.append(str(discrete))
+            
+            tupleload = tuple(preload2)
+            
+            pointer.execute(preload, tupleload)
+            conection.commit()
+        #tableconstructor(conection, fields, list_tables[ind])
+        
+        
+
+def loaddb(coneccion, tuplacarga):
+
+    
+
+    pointer = coneccion.cursor()
+    carga = "INSERT INTO deck(card, type, income, power, agrogen, defenders, mining, refinerie, colonies, labs, notes, force, hits, placement, deckpos, hitted) VALUES (?,?,?,?,?,?,?,?,?,?, ?,?,?,?,?,?)"
+    pointer.execute(carga, tuplacarga)
+    coneccion.commit()
+    
+    
+def loadimages(coneccion, tupleimages):
+    
+    pointer = coneccion.cursor()
+    load = "INSERT INTO images(card, pict, descript) VALUES (?,?,?)"
+    pointer.execute(load, tupleimages)
+    coneccion.commit()
+    
+
+def massiveloader(conector, deck):
+    for x in deck:
+        row = x
+        
+        income = int(row[2])
+        power = int(row[3])
+        agrogen = int(row[4])
+        defenders = int(row[5])
+        mining = int(row[6])
+        refinerie = int(row[7])
+        colonies = int(row[8])
+        labs = int(row[9])
+        force = int(row[11])
+        hits = int(row[12])
+        loadtuple = (row[0], row[1], income, power, agrogen, defenders, mining, refinerie, colonies, labs, row[10], force, hits, row[13], row[14], row[15])
+        
+        loaddb(conector, loadtuple)
+        
+        loadimagestuple = (row[0], row[16], row[17])
+        loadimages(conector, loadimagestuple)
+
+def data_loader(conection, rows, fields):
+    
+    pointer = conection.cursor
+    for row in rows:
+        load_tuple = tuple(row)
+        
+        
+    pass    
+
+
+if __name__ == "__main__":
+    conector = conection_sql()
+    
+    alltablesconstructor(conector)
+    
+    
+    
+    
+    
+        
+    
+    
+    
+    
+
+
+    
+
+    
+
+
