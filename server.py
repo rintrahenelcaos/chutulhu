d849d8e1e0e8be6ccf9906fced5d017283ef45d3
Mazzeo, Leonardö <leomazzeo@gmail.com>
2024-09-04 19:04:43 -0300

    server in the build

--- server.py
+++ server.py
@@ -1,8 +1,10 @@
 import socket
-import _thread
+from _thread import *
 import sys
 
-server = ""
+hostname = socket.gethostname()
+IP_addr = socket.gethostbyname(hostname)
+server = "10.160.4.213"
 port = 5555
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -12,3 +14,32 @@ try:
 
 except socket.error as error:
     str(error)
+    
+sock.listen(2)
+print("Waitng for connection")
+
+def client(conn):
+    
+    reply = ""
+    while True:
+        try: 
+            data = conn.recv(2048)
+            reply = data.decode("utf-8")
+            
+            if not data: 
+                print("disconected")
+                break
+            else: 
+                print("received: ", reply)
+                print("sending: ", reply)
+            
+            conn.sendall(str.encode(reply))
+        
+        except: 
+            break
+
+while True:
+    conn, addr = sock.accept()
+    print("connected to: ", addr)
+    
+    start_new_thread(client, (conn,)) 
