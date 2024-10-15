5e467d715e37b6a5716c7ee712ab6a0e251b423a
Mazzeo, Leonardö <leomazzeo@gmail.com>
2024-10-15 19:04:34 -0300

    setting server and clients again

--- game_network.py
+++ game_network.py
@@ -4,14 +4,14 @@ import pickle
 from pickleobj import Exchange_object
 
 IP = socket.gethostbyname(socket.gethostname())
-IP = "192.168.1.2"
+IP = "10.160.4.213"
 PORT = 5555
 ADDR = (IP, PORT)
 SIZE = 40000
 FORMAT = "utf-8"
 DISCONNECT_MSG = "!DISCONNECT"
 
-connected = True
+#connected = True
 recv_order = [""]
 
 
@@ -91,24 +91,26 @@ class Network:
             self.client.send(cargo.encode(FORMAT))
             #self.client.send(msg.encode(FORMAT))
             #data = pickle.loads(self.client.recv(SIZE))
-            rec_msg = self.client.recv(SIZE).decode(FORMAT)
+            #rec_msg = self.client.recv(SIZE).decode(FORMAT)
             #print("in the network: ", data)
             
             """if cargo == DISCONNECT_MSG:
                 
                 self.client.close()
                 self.thread_listen.join()"""
-            #return data
+            #return rec_msg
+            print(f"recv_order[0]: {recv_order[0]}")
         except socket.error as e:
             print(e)
             
 def listening(client):
-    global recv_order
+    #global recv_order
+    
     while connected:
         msg = client.recv(SIZE).decode(FORMAT)
         if msg != "":
             recv_order[0] = msg
-            #print(f"[RECIEVED]: {recv_order[0]}")
+            print(f"[RECIEVED]: {recv_order[0]}")
             #print(f"[RECIEVED]: msg: {msg}")
 
 
@@ -125,5 +127,45 @@ def main():
             connected = False
     exit()
 
+class Network:
+    def __init__(self) -> None:
+        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        
+    def connect(self, faction):
+        try: 
+            self.client.connect(ADDR) 
+            print(f"[CONNECTED] Client connected to server at {IP}:{PORT}")
+            self.client.send(faction.encode(FORMAT))
+            return self.client.recv(SIZE).decode(FORMAT)
+        except:
+            pass
+        
+    def send_recv(self, cargo):
+        try: 
+            
+                
+            self.client.send(cargo.encode(FORMAT))
+            return self.client.recv(SIZE).decode(FORMAT)
+        except: pass
+        
+    def closing(self):
+        try: 
+            self.client.send(DISCONNECT_MSG.encode(FORMAT))
+            self.client.shutdown()
+            self.client.close()
+        except:
+            print("Unable to disconnect")
+
+def main():
+    net = Network()
+    net.connect("player")
+    conn = True
+    while conn:
+        msg = input(">")
+        print(net.send_recv(input(">")))
+        
+    
+    
+    
 if __name__ == "__main__":
     main()
