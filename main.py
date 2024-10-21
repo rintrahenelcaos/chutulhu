a33129d4e7fb0ec3712464687f7828cc38ddae4d
Leonardo Mazzeo <leomazzeo@gmail.com>
2024-10-21 20:43:30 -0300

    setting server translation finctions2

--- main.py
+++ main.py
@@ -391,7 +391,7 @@ class Main():
                 if enemy_pos != "RECIEVED":
                     code, target, order = recv_msg_translator(enemy_pos)
                     self.orders_interpreter_method(code, target, order)
-                self.net.send_recv("RECIEVED")
+                    self.net.send_recv("RECIEVED")
                 """if code == "VECTORTOGO":
                     for token in self.player_b.player_tokens:
                         if str(token) == target:
