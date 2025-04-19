be568c0ab05e29ad427b71d0fc48688f242006bc
rintrahenelcaos <leomazzeo@gmail.com>
2025-04-18 21:32:08 -0300

    servers

--- main.py
+++ main.py
@@ -839,12 +839,12 @@ class Main():
         self.order_to_send = send_msg_translator("CARDSDRAWN", "faction", drawn_cards)
         print("SELF.ORDER_TO_SEND: CARDS DRAWN ===> ",self.order_to_send)
         self.net.send_only(send_msg_translator("CARDSDRAWN", "faction", drawn_cards))
-        self.recieved_order = self.net.recieve_only()
-        try:
-            code, target, order = recv_msg_translator(self.recieved_order)
-            
-            self.orders_interpreter_method(code, target, order)
-        except: pass
+        #self.recieved_order = self.net.recieve_only()
+        #try:
+        #    code, target, order = recv_msg_translator(self.recieved_order)
+        #    
+        #    self.orders_interpreter_method(code, target, order)
+        #except: pass
         print("pass pregame")
         self.scene = "in_course"
             
